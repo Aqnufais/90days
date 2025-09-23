823982f57c266c4032b317ce95fac11749eeae5a
aqstin <aqnufais45@gmail.com>
2025-09-23 07:31:44 +0530

    js

---
+++ javascript/string.js
@@ -0,0 +1,20 @@
+let str1 = 'hello';
+let str2 = 'World!';
+
+let result = str1.concat(' ',str2);
+
+
+// console.log(result);
+
+let a = "hello";
+
+
+let b = 'world';
+if( 12 > 13){
+    console.log("haha");
+    
+}else{
+
+}
+
+a = "meow";
