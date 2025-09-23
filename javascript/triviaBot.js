50117c2fc723d8015475eb58640f109c9108c5d6
aqstin <aqnufais45@gmail.com>
2025-09-23 10:48:38 +0530

    triviabot

---
+++ javascript/triviaBot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide");
+
+let botName = "triviaBot";
+let botLocation = "universe";
+let favoriteLanguage = "javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is my favorite programming language";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is also my favorite programming language";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is also my favorite programming language";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodby! - " + botName + " form " + botLocation+ ".") ;
+
+
