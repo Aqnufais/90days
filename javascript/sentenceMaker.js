c811af3fbf8c86016801d3a28e2410ad9ef19420
aqstin <aqnufais45@gmail.com>
2025-09-23 11:07:46 +0530

    sentenceMker

---
+++ javascript/sentenceMaker.js
@@ -0,0 +1,31 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+let firstStory;
+
+adjective = "brave";
+noun = "dragon";
+verb = "dancing";
+place = "castle";
+adjective2 = "enormous";
+noun2 = "cupcakes";
+
+firstStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrills that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "silly";
+noun = "robot";
+noun2 = "pizzas";
+place = "laboratory";
+adjective2 = "tiny";
+verb = "singing";
+
+
+let secondStory = "Once upon a time, there was a(n) " + adjective +" " +noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrills that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
