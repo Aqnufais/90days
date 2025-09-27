fa521ac2e78065eebf66b5b22cb5b703bc29906b
aqstin <aqnufais45@gmail.com>
2025-09-27 21:15:40 +0530

    js

---
+++ javascript/strings.js
@@ -0,0 +1,10 @@
+let greeting = "Hello, World!";
+let uppercaseGreeting = greeting.toUpperCase();
+console.log(uppercaseGreeting);
+
+let shout = "I AM LEARNING JAVASCRIPT!";
+let lowercaseShout = shout.toLowerCase();
+console.log(lowercaseShout); 
+
+let word = "Test ";
+console.log(word.repeat(5));
