7fc5cb9237c96cb1eb75ba489166d8ab40e00fda
aqstin <aqnufais45@gmail.com>
2025-10-02 15:07:12 +0530

    Mathbot

--- javascript/string.js
+++ javascript/string.js
@@ -1,1 +1,17 @@
-console.log(2 / 0);
+// console.log(2 / 0);
+
+
+const randomNum = Math.random();
+
+// console.log(randomNum);
+
+const smallest = Math.min(2, 5, 3, 9);
+// console.log(smallest); // 1
+
+const largest = Math.max(1, 5, 3, 19);
+// console.log(largest); 
+
+// console.log(Math.ceil(6.3));
+
+const randomNumBtw1And20 = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumBtw1And20);
